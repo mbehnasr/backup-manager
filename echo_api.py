97e26c10f3e52e15b9937f54aca0ddb4bd1d3002
m.m.behnasr.2000 <m.m.behnasr.2000@gmail.com>
2023-05-30 13:42:40 +0330

    fix : fix flask for working

--- echo_api.py
+++ echo_api.py
@@ -3,20 +3,19 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def echo():
-    message = request.get_data().decode('utf-8')
-
-    # Write the message to a file
-    filename = f"/var/lib/echo_api/{message}"
-    with open(filename, 'w') as file:
+@app.route('/echo/<message>', methods=['GET'])
+def echo(message):
+    # create the directoyr i fit doesn't exist
+    directory = '/var/lib/echo_api/'
+    os.makedirs(directory, exist_ok=True)
+     # Save the message to a file
+    file_path = os.path.join(directory, message)
+    with open(file_path, 'w') as file:
         file.write(message)
 
+         # Return the echoed message
     return message
 
 if __name__ == '__main__':
-    # Ensure the directory exists
-    os.makedirs('/var/lib/echo_api/', exist_ok=True)
-
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='localhost', port=8080)
+                                    
